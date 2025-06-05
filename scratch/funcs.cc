d22a0b8638afbc5815ce15bf254b4c6af259122d
Kristina <yurginapoet@gmail.com>
2025-06-05 20:19:43 +0700

    added funcs

--- scratch/funcs.cc
+++ scratch/funcs.cc
@@ -5,13 +5,17 @@ double f(nd &n, double t, int flag)
   switch (flag)
   {
   case 1:
-    return 1;
+    // return 1;
+    return 1 - 0;
   case 2:
-    return 2 * t;
+    // return 2 * t;
+    return 2 * t - 0;
   case 3:
-    return 3 * t * t;
+    // return 3 * t * t;
+    return 3 * t * t - 0;
   case 4:
-    return 4 * t * t * t;
+    // return 4 * t * t * t;
+    return 4 * t * t * t - 0;
   case 5:
     return -sin(t);
   default:
@@ -25,14 +29,17 @@ double u(nd &n, double t, int flag)
   switch (flag)
   {
   case 1:
-    return t;
-    // return n.r * n.r + n.z + t;
+    // return t;
+    return /* n.r + */ n.z + t;
   case 2:
-    return t * t;
+    // return t * t;
+    return /* n.r + */ n.z + t * t;
   case 3:
-    return t * t * t;
+    // return t * t * t;
+    return /* n.r + */ n.z + t * t * t;
   case 4:
-    return t * t * t * t;
+    // return t * t * t * t;
+    return /* n.r + */ n.z + t * t * t * t;
   case 5:
     return cos(t);
 
